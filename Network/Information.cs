bddc041b09959bca66a2a14b5ffbf859858927e3
Idan Sahar <idansahar22@gmail.com>
2023-04-15 12:14:21 +0300

    Started Network class library

---
+++ Network/Information.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace Network;
+
+/// <summary>
+/// Contains information about this client's (local computer's) network
+/// </summary>
+public static class Information
+{
+    /// <summary>
+    /// The port to use for the client's server
+    /// </summary>
+    public const int Port = 49152;
+    
+    private static IPHostEntry? _hostEntry;
+    /// <summary>
+    /// The <see cref="IPHostEntry"/> for this client's network
+    /// </summary>
+    public static IPHostEntry HostEntry => _hostEntry ??= Dns.GetHostEntry(Dns.GetHostName());
+    
+    private static IPAddress? _localAddress;
+    /// <summary>
+    /// The <see cref="IPAddress"/> of this client
+    /// </summary>
+    public static IPAddress LocalAddress => _localAddress ??= HostEntry.AddressList[0];
+    
+    private static IPEndPoint? _localEndPoint;
+    /// <summary>
+    /// The <see cref="IPEndPoint"/> of this client's connection
+    /// </summary>
+    public static IPEndPoint LocalEndPoint => _localEndPoint ??= new IPEndPoint(LocalAddress, Port);
+}
